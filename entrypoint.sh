b01e7a5e4683d385e38f05fc7866195e7739f847
ncc-74205 <40716826+ncc-74205@users.noreply.github.com>
2019-04-08 15:12:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,6 @@
 # Copyright (c) Jupyter Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-cd /home/jovyan/jupyter_edu_data
-git reset --hard
-git pull || true
-cd /home/jovyan
-
 mysqld_safe --skip-grant-tables &
 
 tini -g -- "$@"
